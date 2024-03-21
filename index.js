4fdd0bbf3802b20c389ebea18074da5ddd632789
achoclate <nyabokeann99@gmail.com>
2024-03-21 00:03:15 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+const currentUser = 'ann achoki'
+let welcomeMessage = 'Welcome to Flatbook, ';
+welcomeMessage = 'currentUser';
+welcomeMessage = 'Welcome to Flatbook, ann achoki!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+const upperCaseCurrentUser = 'ANN ACHOKI';
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ANN ACHOKI!';
+let shortGreeting = 'Welcome, ';
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+shortGreeting = 'Welcome, a!';
+
+
+
+
+
+
+
+
+
+
+
+
+
